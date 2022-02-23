87555a62272387d641a81b64297eb65fcd75d950
Alexey Permyakov <patric.ynos@gmail.com>
2022-02-23 18:20:23 +0300

    cart display and search input added

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+const cart = function () {
+  const cartBtn = document.querySelector('.button-cart'),
+    cart = document.querySelector('#modal-cart'),
+    closeBtn = cart.querySelector('.modal-close');
+
+  cartBtn.addEventListener('click', function () {
+    cart.style.display = 'flex';
+  });
+
+  closeBtn.addEventListener('click', function () {
+    cart.style.display = '';
+  });
+};
+
+cart();
