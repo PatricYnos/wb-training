3d10707b22e7cddc085abf639072f7714cad826e
Alexey Permyakov <patric.ynos@gmail.com>
2022-02-24 17:41:09 +0300

    work with localstorage

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,6 +1,18 @@
 const getGoods = () => {
-  fetch('https://glo-wb-38b0a-default-rtdb.firebaseio.com/db.json')
-    .then(res => res.json())
-    .then(data => console.log(data));
+  const links = document.querySelectorAll('.navigation-link');
+
+  const getData = () => {
+    fetch('/db/db.json')
+      .then(res => res.json())
+      .then(data => localStorage.setItem('data', JSON.stringify(data)));
+  };
+
+  links.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      getData();
+    });
+  });
+
 };
 getGoods();
