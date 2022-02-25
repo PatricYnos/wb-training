3676f5326b0c670cda20eaf0425105c5294f806f
Alexey Permyakov <patric.ynos@gmail.com>
2022-02-25 13:22:03 +0300

    add card generation

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,18 +1,58 @@
 const getGoods = () => {
   const links = document.querySelectorAll('.navigation-link');
 
-  const getData = () => {
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector('.long-goods-list');
+    goodsContainer.innerHTML = '';
+
+    goods.forEach(good => {
+      const goodBlock = document.createElement('div');
+
+      goodBlock.classList.add('col-lg-3', 'col-sm-6');
+
+      goodBlock.innerHTML = `
+          <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>
+						<p class="goods-description">${good.description}</p>
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+      `;
+
+      goodsContainer.append(goodBlock);
+    });
+  };
+
+  const getData = (value, category) => {
     fetch('/db/db.json')
       .then(res => res.json())
-      .then(data => localStorage.setItem('data', JSON.stringify(data)));
+      .then(data => {
+        const array = category ? data.filter(item => item[category] === value) : data;
+        localStorage.setItem('data', JSON.stringify(array));
+
+        if (window.location.pathname !== '/goods.html') {
+          window.location.href = '/goods.html';
+        } else {
+          renderGoods(array);
+        }
+      });
   };
 
   links.forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
-      getData();
+      const linkValue = link.textContent,
+        category = link.dataset.field;
+
+      getData(linkValue, category);
     });
   });
 
+  if (localStorage.getItem('data') && window.location.pathname === '/goods.html') {
+    renderGoods(JSON.parse(localStorage.getItem('data')));
+  }
 };
 getGoods();
